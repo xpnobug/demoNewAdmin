f667be5c4fc7786fe75262160526b3f930f69474
couei <669307582@qq.com>
2024-07-28 05:17:17 +0800

    feat: 配置 MinioClient 管理器以注入配置信息

--- demo-service/src/main/java/com/newadmin/demoservice/base/upload/MinioClientManager.java
+++ demo-service/src/main/java/com/newadmin/demoservice/base/upload/MinioClientManager.java
@@ -2,7 +2,10 @@ package com.newadmin.demoservice.base.upload;
 
 import io.minio.MinioClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class MinioClientManager {
 
     @Value("${minio.endpoint}")
@@ -14,6 +17,7 @@ public class MinioClientManager {
     @Value("${minio.secretKey}")
     private String secretKey;
 
+    @Bean
     public MinioClient createMinioClient() {
         return MinioClient.builder()
             .endpoint(endpoint)
