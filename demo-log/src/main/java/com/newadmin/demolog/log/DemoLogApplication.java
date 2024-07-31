9a6d1a9839d4f92f2b737d679e1fd398f54abd31
couei <669307582@qq.com>
2024-07-31 21:31:20 +0800

    fix(打包问题): 移除日志模块的Java版本指定和Spring Boot Maven插件配置，新增DemoLogApplication启动类。
    Signed-off-by: couei <669307582@qq.com>

---
+++ demo-log/src/main/java/com/newadmin/demolog/log/DemoLogApplication.java
@@ -0,0 +1,12 @@
+package com.newadmin.demolog.log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoLogApplication.class, args);
+    }
+}
